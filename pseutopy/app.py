795edcc98a3e8067cae4e2daeac4735edac2f340
Patrick Wang <patrick.wang@isep.fr>
2020-06-12 23:38:08 +0200

    Added Error 404 handler

--- pseutopy/app.py
+++ pseutopy/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, g, redirect, url_for
+from flask import Flask, request, g, redirect, url_for, render_template
 from flask_babel import Babel
 from config import Config
 # import and register blueprints
@@ -25,5 +25,11 @@ def get_locale():
 
 @app.route('/')
 def home():
-    g.lang_code = request.accept_languages.best_match(app.config['LANGUAGES'])
+    g.lang_code = 'en'
     return redirect(url_for('multilingual.index'))
+
+
+@app.errorhandler(404)
+def not_found(e):
+    print("Message", e)
+    return render_template("error404.html")
--- pseutopy/blueprints/multilingual/routes.py
+++ pseutopy/blueprints/multilingual/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, Blueprint, g, redirect, request, current_app, abort, url_for
-from flask_babel import _, refresh
+from flask import render_template, Blueprint, g, redirect, request, current_app, \
+    url_for
+from flask_babel import _
 
 multilingual = Blueprint('multilingual', __name__,
                          template_folder='templates', url_prefix='/<lang_code>')
@@ -15,16 +16,35 @@ def pull_lang_code(endpoint, values):
     g.lang_code = values.pop('lang_code')
 
 
-@multilingual.route('/')
+@multilingual.route('/home', defaults={'lang_code': 'en'})
+@multilingual.route('/accueil', defaults={'lang_code': 'fr'})
 def index():
-    return render_template('multilingual/index.html')
+    return render_template('multilingual/index.html', title=_('Home'))
 
 
-@multilingual.route('/editor')
+@multilingual.route('/editor', defaults={'lang_code': 'en'})
+@multilingual.route('/editeur', defaults={'lang_code': 'fr'})
 def editor():
     return render_template('multilingual/editor.html',
+                           title=_('Editor'),
                            variables="Variables",
                            data_structures="Data structures",
                            maths="Maths",
                            control_structures="Control structures",
                            functions="Functions")
+
+
+@multilingual.before_request
+def before_request():
+    if g.lang_code not in current_app.config['LANGUAGES']:
+        adapter = current_app.url_map.bind('')
+        try:
+            endpoint, args = adapter.match('/en' +
+                                           request.full_path.rstrip('/ ?'))
+            return redirect(url_for(endpoint, **args), 301)
+        except:
+            return render_template('multilingual/404.html')
+    dfl = request.url_rule.defaults
+    if 'lang_code' in dfl:
+        if dfl['lang_code'] != request.full_path.split('/')[1]:
+            return render_template('multilingual/404.html')

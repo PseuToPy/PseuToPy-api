c4d00b5f9a09f0035630316d1418d94a0c2d7b4e
Patrick Wang <patrick.wang@isep.fr>
2020-06-19 17:20:47 +0200

    Complete interface in English

--- pseutopy/blueprints/multilingual/routes.py
+++ pseutopy/blueprints/multilingual/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, Blueprint, g, redirect, request, current_app, \
     url_for
 from flask_babel import _
+from werkzeug.exceptions import BadRequest
+
+from pseutopy import app
 
 multilingual = Blueprint('multilingual', __name__,
                          template_folder='templates', url_prefix='/<lang_code>')
@@ -16,35 +19,33 @@ def pull_lang_code(endpoint, values):
     g.lang_code = values.pop('lang_code')
 
 
-@multilingual.route('/home', defaults={'lang_code': 'en'})
-@multilingual.route('/accueil', defaults={'lang_code': 'fr'})
+@multilingual.route('/home')
 def index():
     return render_template('multilingual/index.html', title=_('Home'))
 
 
-@multilingual.route('/editor', defaults={'lang_code': 'en'})
-@multilingual.route('/editeur', defaults={'lang_code': 'fr'})
+@multilingual.route('/editor')
 def editor():
-    return render_template('multilingual/editor.html',
-                           title=_('Editor'),
-                           variables="Variables",
-                           data_structures="Data structures",
-                           maths="Maths",
-                           control_structures="Control structures",
-                           functions="Functions")
+    return render_template('multilingual/editor.html')
+
+
+@multilingual.route('/error404')
+def page_not_found():
+    return render_template('multilingual/error404.html', title=_('Error404'))
 
 
 @multilingual.before_request
 def before_request():
+    """
+    This is where the URL of type <lang>/<home> will be managed
+    :return:
+    """
     if g.lang_code not in current_app.config['LANGUAGES']:
-        adapter = current_app.url_map.bind('')
-        try:
-            endpoint, args = adapter.match('/en' +
-                                           request.full_path.rstrip('/ ?'))
-            return redirect(url_for(endpoint, **args), 301)
-        except:
-            return render_template('multilingual/404.html')
+        print("TEST")
+        g.lang_code = 'en'
+        return render_template('multilingual/error404.html')
     dfl = request.url_rule.defaults
     if 'lang_code' in dfl:
+        print("DEBUG TEST")
         if dfl['lang_code'] != request.full_path.split('/')[1]:
-            return render_template('multilingual/404.html')
+            return render_template('multilingual/error404.html')

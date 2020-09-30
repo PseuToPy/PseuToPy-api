09dbbe5d4da69c70e0082005c63c1d66a20eff70
tiboxman <tibogrouet@hotmail.fr>
2020-09-30 11:17:24 +0200

    working logger in database based on Config variarible url

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Config(object):
     DEBUG = True
     LANGUAGES = ['en', 'fr']
+    MONGO_URI = 'mongodb://localhost:27017/PseudoToPy'
--- src/app.py
+++ src/app.py
@@ -4,16 +4,21 @@ from flask_babel import Babel, _
 from werkzeug.exceptions import HTTPException
 from werkzeug.debug import DebuggedApplication
 from pseutopy.pseutopy import PseuToPy
+from pymongo import MongoClient
+from datetime import datetime
+from config import Config
 import astor
 
-
-from config import Config
 # import and register blueprints
 from src.blueprints.multilingual import multilingual
 
 # set up application
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# initialize Mongodb instance
+mongo = MongoClient(app.config['MONGO_URI'])
+
 app.register_blueprint(multilingual)
 if app.debug:
     app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
@@ -54,13 +59,25 @@ def convert():
             pseutopy = PseuToPy()
             python_ast = pseutopy.convert_from_string(instructions)
             python_instructions = astor.to_source(python_ast)
+            log(params) # call Mongo Log method
             return jsonify(status=0, response=python_instructions)
         except Exception:
+            params['error'] = "Pseudocode parsing error"
+            log(params) # call Mongo Log method
             return jsonify(status=1, response= _("Pseudocode parsing error"))
     else:
+        params['error'] = "Convert error"
+        log(params) # call Mongo Log method
         return jsonify(status=1, response=_("Convert error"))
+
 
 
 @app.errorhandler(HTTPException)
 def http_error_handler(e):
     return render_template('error404.html')
+
+# Log in database 'PseudoToPy' in collection 'logs'
+# params - data: query params from /convert 
+def log(data):
+    data['timestamp'] = datetime.now() # Add timestamp field
+    return mongo.PseuToPy.logs.insert_one(data) # insert in database

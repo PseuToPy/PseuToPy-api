a6cec0c5cc585969c5676d482d8919e910c00580
SebEdena <viguier.sebastien@orange.fr>
2021-06-03 18:16:19 +0200

    correct error handling + flask cors security update

--- src/services/convert.py
+++ src/services/convert.py
@@ -1,6 +1,8 @@
 from pseutopy import pseutopy 
 from enum import Enum
 
+default_message = "An error occured: Unable to parse the input. Please check that your input is correct."
+
 class ConversionStatus(Enum):
     SUCCESS = "SUCCESS"
     ERROR = "ERROR"
@@ -8,9 +10,9 @@ class ConversionStatus(Enum):
 def convert(instructions, language):
     try:
         print(instructions)
-        r = pseutopy.PseuToPy(language).convert_from_string(instructions)
-        if r == "An error occured: Unable to parse the input. Please check that your input is correct.":
-            return "", ConversionStatus.ERROR, "{}".format(e)
+        r = pseutopy.PseuToPy(language).convert_from_string(instructions).rstrip()
+        if r == default_message:
+            return "", ConversionStatus.ERROR, "{}".format(r)
         return r, ConversionStatus.SUCCESS, "Converted successfully!"
     except Exception as e:
         return "", ConversionStatus.ERROR, "{}".format(e)
